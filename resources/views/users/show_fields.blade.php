ec2861e5361ba17326f57ea010194be9dc79d49f
david <7etan17@gmail.com>
2023-03-03 22:43:17 +0200

    edit users

--- resources/views/users/show_fields.blade.php
+++ resources/views/users/show_fields.blade.php
@@ -6,10 +6,9 @@
                     <div class="image image-circle image-lg-small">
                         <img src="{{$user->profile_image}}" alt="user">
                     </div>
-                    <div class="ms-0 ms-md-10 mt-5 mt-sm-0">
+                    <div class="ms-0 {{ app()->getLocale() == 'ar' ? 'me-md-10' : 'ms-md-10' }} mt-5 mt-sm-0">
                         <h2>{{$user->full_name}}</h2>
-                        <a href="mailto:michaelfrankin@gmail.com"
-                           class="text-gray-600 text-decoration-none fs-4">
+                        <a href="mailto:michaelfrankin@gmail.com" class="text-gray-600 text-decoration-none fs-4">
                             {{$user->email}}
                         </a>
                     </div>
@@ -22,7 +21,7 @@
     <ul class="nav nav-tabs mb-5 pb-1 overflow-auto flex-nowrap text-nowrap" id="myTab" role="tablist">
         <li class="nav-item position-relative me-7 mb-3" role="presentation">
             <button class="nav-link active p-0" id="overview-tab" data-bs-toggle="tab" data-bs-target="#overview"
-                    type="button" role="tab" aria-controls="overview" aria-selected="true">
+                type="button" role="tab" aria-controls="overview" aria-selected="true">
                 Overview
             </button>
         </li>
@@ -32,14 +31,17 @@
             <div class="tab-content" id="myTabContent">
                 <div class="tab-pane fade show active" id="overview" role="tabpanel" aria-labelledby="overview-tab">
                     <div class="row">
-                        <div class="col-sm-6 d-flex flex-column mb-md-10 mb-5 {{ empty($user->contact) ? 'd-none' : ''}}">
+                        <div
+                            class="col-sm-6 d-flex flex-column mb-md-10 mb-5 {{ empty($user->contact) ? 'd-none' : ''}}">
                             <label for="name" class="pb-2 fs-4 text-gray-600">{{__('messages.user.contact_number')}}
                                 :</label>
                             <span class="fs-4 text-gray-800">{{$user->contact}}</span>
                         </div>
                         <div class="col-sm-6 d-flex flex-column mb-md-10 mb-5">
                             <label for="name" class="pb-2 fs-4 text-gray-600">Registered Date:</label>
-                            <span class="fs-4 text-gray-800">{{\Carbon\Carbon::parse($user->created_at)->translatedFormat('jS \of F Y')}}</span>
+                            <span
+                                class="fs-4 text-gray-800">{{\Carbon\Carbon::parse($user->created_at)->translatedFormat('jS
+                                \of F Y')}}</span>
                         </div>
 
                     </div>
